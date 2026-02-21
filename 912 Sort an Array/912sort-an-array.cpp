c5e3a753468b61c6feb2aa57ce8a2ad3403d4d13
thejashree15 <thejashreet3@@gmail.com>
2026-02-21 15:38:18 +0000

    Code challenge submission for question: 912 Sort an Array

---
+++ 912 Sort an Array/912sort-an-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            } else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+    }
+
+    void ms(vector<int>& arr, int low, int high) {
+        if (low >= high) return;
+        int mid = (low + high) / 2;
+        ms(arr, low, mid);
+        ms(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        ms(nums, 0, nums.size() - 1);
+        return nums;
+    }
+};
