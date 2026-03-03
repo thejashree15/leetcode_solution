342a7a13683d3bf6050667064972048ea09d153d
thejashree15 <thejashreet3@@gmail.com>
2026-03-03 06:05:35 +0000

    Code challenge submission for question: 162 Find Peak Element

---
+++ 162 Find Peak Element/162find-peak-element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int maxIndex=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[maxIndex])
+            
+        maxIndex=i;
+        }
+          return maxIndex;
+    }
+   
+};
