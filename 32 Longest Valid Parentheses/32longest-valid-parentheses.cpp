39c5650f0da54baf2e0596f52a1662f2e7d4e63e
thejashree15 <thejashreet3@@gmail.com>
2026-04-15 18:26:59 +0000

    Code challenge submission for question: 32 Longest Valid Parentheses

---
+++ 32 Longest Valid Parentheses/32longest-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);   // base index
+        int maxLen = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+
+                if(st.empty()) {
+                    st.push(i);
+                } else {
+                    maxLen = max(maxLen, i - st.top());
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
