e61f50308ea986d12e1f34a69e76a393ab38ee36
thejashree15 <thejashreet3@@gmail.com>
2026-05-11 13:48:28 +0000

    Code challenge submission for question: 9 Palindrome Number

---
+++ 9 Palindrome Number/9palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev=0;
+        int original=x;
+        if(x<0)
+        return false;
+        while(x!=0)
+        {
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        return original==rev;
+        
+        
+    }
+};
