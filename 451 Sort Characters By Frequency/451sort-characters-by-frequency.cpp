e126389a985e06ac1535bd4779f92daf36cc70d2
thejashree15 <thejashreet3@@gmail.com>
2026-08-03 07:13:07 +0000

    Code challenge submission for question: 451 Sort Characters By Frequency

---
+++ 451 Sort Characters By Frequency/451sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        unordered_map<char, int> freq;
+
+        for (char ch : s)
+            freq[ch]++;
+
+        vector<vector<char>> bucket(s.size() + 1);
+
+        for (auto it : freq)
+            bucket[it.second].push_back(it.first);
+
+        string ans = "";
+
+        for (int i = s.size(); i >= 1; i--) {
+
+            for (char ch : bucket[i]) {
+
+                for (int j = 0; j < i; j++) {
+                    ans += ch;
+                }
+
+            }
+        }
+
+        return ans;
+    }
+};
